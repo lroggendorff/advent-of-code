6bc707dc354ddae397f8062254cb1981c68f20e5
Lance Roggendorff <lance.roggendorff@myemma.com>
2025-12-05 12:12:42 -0600

    2025 Day 1 part 1.

--- aoc2025/days/day1.py
+++ aoc2025/days/day1.py
@@ -2,11 +2,27 @@ import pathlib
 
 
 def answer(data):
-    return data
+    commands = [{ "direction": c[0], "amount": int(c[1:]) } for c in data.split("\n")]
+
+    code = 0
+    value = 50
+    for command in commands:
+        direction = command["direction"]
+        amount = command["amount"]
+
+        if direction == "L":
+            value = (value - amount) % 100
+        else:
+            value = (value + amount) % 100
+
+        if value == 0:
+            code += 1
+
+    return code
 
 
 def answer_part_two(data):
-    pass
+    return 0
 
 
 if __name__ == "__main__":
--- tests/aoc2025/test_day1.py
+++ tests/aoc2025/test_day1.py
@@ -6,9 +6,18 @@ from aoc2025.days.day1 import (
 
 def test_answer():
     data = """
-
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
     """
-    assert answer(data.strip()) == 0
+    assert answer(data.strip()) == 3
 
 
 def test_answer_part_two():
